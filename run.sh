ee35c7d541aa2608792ccf8d328d128061066182
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-09-23 16:01:36 -0700

    update metadata; jax 0.7.2

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 TPU_NAME="$1"
 shift
 SSH_FLAGS='-A -o ForwardAgent=yes'
-COMMANDS="if [ ! -d \"nanocrate\" ]; then git clone git@github.com:sdbuch/nanocrate; fi \
-    && cd nanocrate \
+COMMANDS="if [ ! -d \"baremetal-gpt\" ]; then git clone git@github.com:sdbuch/baremetal-gpt; fi \
+    && cd baremetal-gpt \
     && git pull \
     && git checkout train-loop-first-cut \
     && uv run --extra tpu train.py $@"
