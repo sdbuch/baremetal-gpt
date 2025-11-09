1e3277e911041501d669f176cd879ad4f6abcf4f
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-11-09 13:37:36 -0800

    debug

--- src/bmgpt/train.py
+++ src/bmgpt/train.py
@@ -113,6 +113,9 @@ def main(config: Config):
     prev_metrics = None
     with Logger(config) as logger:
         for step in range(config.num_steps):
+            q = next(batch)
+            print(q)
+            print(jax.typeof(q))
             cur_metrics, train_state = train_step(config, next(batch), train_state)
             log_metrics, prev_metrics = prev_metrics, cur_metrics
             if log_metrics:
