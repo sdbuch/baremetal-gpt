d24c579be8d9c3ac02798c7b20b806648a7a881a
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-11-09 14:01:20 -0800

    debug

--- src/bmgpt/train.py
+++ src/bmgpt/train.py
@@ -115,7 +115,7 @@ def main(config: Config):
         for step in range(config.num_steps):
             q = next(batch)
             print(q[0].addressable_shards)
-            print(q[0].addressable_data().shape)
+            print(q[0].addressable_data(0).shape)
             cur_metrics, train_state = train_step(config, next(batch), train_state)
             log_metrics, prev_metrics = prev_metrics, cur_metrics
             if log_metrics:
