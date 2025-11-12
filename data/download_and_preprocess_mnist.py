41d45c43604590e1ff5ba0e974ef70502d1194ae
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-11-12 15:25:38 -0800

    update mnist preprocessing, for patchifying

--- data/download_and_preprocess_mnist.py
+++ data/download_and_preprocess_mnist.py
@@ -6,6 +6,8 @@ from datasets import load_dataset
 
 # Configure your HF_HOME and HF_TOKEN as necessary externally
 
+PATCH_SIZE = 4
+
 # Load and array-ify data
 dataset = load_dataset("ylecun/mnist")
 train = dataset["train"]
@@ -17,11 +19,22 @@ test_labels = jnp.array(test["label"])
 
 
 # Preprocess the data
+assert train_imgs.shape[-1] % PATCH_SIZE == 0, "PATCH_SIZE needs to divide image size"
+
+
+@jax.jit
 def preprocess_img(im: jax.Array) -> jax.Array:
+    # TODO: if adapting this to multichannel img, update this
     # Convert to bf16
     im = im.astype(jnp.bfloat16)
     # Rescale to [0, 1]
     im /= 255.0
+    # Patchify (non-overlapping)
+    num_patch_H = im.shape[-2] // PATCH_SIZE
+    num_patch_W = im.shape[-1] // PATCH_SIZE
+    im = im.reshape(num_patch_H, PATCH_SIZE, num_patch_W, PATCH_SIZE)
+    im = jnp.permute_dims(im, (0, 2, 1, 3))
+    im = im.reshape(-1, PATCH_SIZE**2)
     return im
 
 
@@ -33,6 +46,14 @@ train_imgs, test_imgs = jax.tree.map(
 )
 
 # Save data
-os.makedirs("data/mnist", exist_ok=True)
-jnp.savez("data/mnist/mnist_train.npz", images=train_imgs, labels=train_labels)
-jnp.savez("data/mnist/mnist_test.npz", images=test_imgs, labels=test_labels)
+os.makedirs(f"data/mnist/patch_{PATCH_SIZE}x{PATCH_SIZE}", exist_ok=True)
+jnp.savez(
+    f"data/mnist/patch_{PATCH_SIZE}x{PATCH_SIZE}/mnist_train.npz",
+    images=train_imgs,
+    labels=train_labels,
+)
+jnp.savez(
+    f"data/mnist/patch_{PATCH_SIZE}x{PATCH_SIZE}/mnist_test.npz",
+    images=test_imgs,
+    labels=test_labels,
+)
