699fd66a2cf1990077b3de0e7f3451f077649e20
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-11-15 21:57:45 -0800

    fix bug

--- src/bmgpt/sample.py
+++ src/bmgpt/sample.py
@@ -20,7 +20,7 @@ def sample_one_token(
     temperature: float,
 ):
     """Expects seq and cache_in to have no batch axis."""
-    y, cache_out = _transformer(config, params, seq, cache_in, cache_size)
+    y, cache_out = _transformer(config, None, params, seq, cache_in, cache_size)
     logits = y.astype(config.model.compute_dtype.value)
     cache_size = cache_size + seq.shape[-1]  # TODO: this should maybe be internal
     next_token = jnp.array((jax.random.categorical(key, logits[-1] / temperature),))
