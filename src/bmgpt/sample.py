b4ba96d5b3b765085a7e90a5cbc4ea08533b55c7
sdbuch <19997120+sdbuch@users.noreply.github.com>
2025-11-23 14:12:46 -0800

    try to fix tracer issue by pushing 1 level down

--- src/bmgpt/sample.py
+++ src/bmgpt/sample.py
@@ -7,22 +7,23 @@ from bmgpt.config import Config
 from bmgpt.model import CacheParams, Transformer, _transformer
 
 
-def sample_one_token(
+def sample_one_prompt(
   config: Config,
   key,
   kernel,
   params: Transformer,
   seq: jax.Array,
   cache_in: jax.Array,
-  cache_params: CacheParams,
+  cache_size: int,
   temperature: float,
 ):
   """Expects seq and cache_in to have no batch axis."""
+  cache_params = CacheParams(enabled=True, size=cache_size)
   y, cache_out = _transformer(config, kernel, params, seq, cache_in, cache_params)
   logits = y.astype(config.model.compute_dtype.value)
-  cache_params = CacheParams(cache_params.enabled, cache_params.size + seq.shape[-1])
+  cache_size = cache_size + seq.shape[-1]
   next_token = jnp.array((jax.random.categorical(key, logits[-1] / temperature),))
-  return next_token, cache_out, cache_params
+  return next_token, cache_out, cache_size
 
 
 @partial(jax.jit, static_argnums=(2,), donate_argnums=(5,))
@@ -34,34 +35,39 @@ def generate(
   prompt: jax.Array,
   cache: jax.Array,
   cache_size: int,
-) -> tuple[jax.Array, jax.Array, CacheParams]:
+) -> tuple[jax.Array, jax.Array, int]:
   """Expects prompt and cache to have no batch axis."""
   # Note: next_token is a length-1 sequence throughout
   # Prefill
-  cache_params = CacheParams(enabled=True, size=cache_size)
   key, sk = jax.random.split(key)
-  next_token, cache, cache_params = sample_one_token(
-    config, sk, kernel, params, prompt, cache, cache_params, config.inference.temperature
+  next_token, cache, cache_size = sample_one_prompt(
+    config,
+    sk,
+    kernel,
+    params,
+    prompt,
+    cache,
+    cache_size,
+    config.inference.temperature,
   )
   prefill = jnp.concatenate((prompt, next_token))
 
   # Generation loop
-  def loop_fn(next_token__cache__cache_params, key):
-    next_token, cache, cache_params = sample_one_token(
+  def loop_fn(next_token__cache__cache_size, key):
+    next_token, cache, cache_size = sample_one_prompt(
       config,
       key,
       kernel,
       params,
-      *next_token__cache__cache_params,
+      *next_token__cache__cache_size,
       config.inference.temperature,
     )
-    cache_params = CacheParams(enabled=True, size=cache_params.size)
-    return (next_token, cache, cache_params), next_token[0]
+    return (next_token, cache, cache_size), next_token[0]
 
   keys = jax.random.split(key, config.inference.max_tokens_to_generate)
   (next_token, cache, cache_params), output = jax.lax.scan(
-    loop_fn, (next_token, cache, cache_params), keys
+    loop_fn, (next_token, cache, cache_size), keys
   )
   output = jnp.concatenate((prefill, output))
 
-  return output, cache, cache_params
+  return output, cache, cache_size
