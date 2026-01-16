precision fixes (wrap gathers in fp32 casts)

src/bmgpt/losses.py
@@ -82,9 +82,11 @@ def fused_softmax_cross_entropy(
   # TODO: does the usual logit scaling make sense for outputs too?
   # lse = lse_sharded(lse_kernel, q / d**0.25, k / d**0.25, v, segment_ids)
   lse = lse_sharded(lse_kernel, q, k).squeeze(0)
-  per_token_unembs = w_unemb.at[targets].get(out_sharding=jax.P(*config.sharding.data))
+  w_unemb_f32 = w_unemb.astype(jnp.float32) # perform the bwd scatter-add in fp32
+  target_unembs = w_unemb_f32.at[targets].get(out_sharding=jax.P(*config.sharding.data))
+  target_unembs = target_unembs.astype(w_unemb.dtype)
   label_logits = jnp.einsum(
-    "td,td->t", outputs, per_token_unembs, preferred_element_type=jnp.float32
+    "td,td->t", outputs, target_unembs, preferred_element_type=jnp.float32
   )
   loss = lse - label_logits
   if reduce:

src/bmgpt/model.py
@@ -275,7 +275,9 @@ def init_embedding_discrete(config: Config, key) -> EmbeddingDiscrete:
 
 
 def _embedding_discrete(config: Config, params: EmbeddingDiscrete, tokens: jax.Array):
-  emb = params.w.at[tokens].get(out_sharding=jax.P(*config.sharding.res_stream))
+  w_f32 = params.w.astype(jnp.float32) # upcast to perform the bwd scatter-add in fp32
+  emb = w_f32.at[tokens].get(out_sharding=jax.P(*config.sharding.res_stream))
+  emb = emb.astype(config.model.param_dtype.value)
   if config.model.use_bias_embeddings:
     emb += params.bias
   return emb

src/bmgpt/train.py
@@ -114,8 +114,8 @@ def main(config: Config):
       loss, grad = jax.value_and_grad(loss_fn)(state.params, microbatch)
       return (loss_accum + loss, jax.tree.map(jnp.add, grad_accum, grad)), loss
 
-    zeros_like_fp32 = partial(jnp.zeros_like, dtype=jnp.float32)
-    carry = (jnp.zeros(()), jax.tree.map(zeros_like_fp32, state.params))
+    zeros_like_f32 = partial(jnp.zeros_like, dtype=jnp.float32)
+    carry = (jnp.zeros(()), jax.tree.map(zeros_like_f32, state.params))
     (loss, grad), raw_losses = jax.lax.scan(gradient_accum, carry, batch)
     assert raw_losses.dtype == jnp.float32
     # NOTE: breaks if per-token loss masking introduced (see unsloth blog)